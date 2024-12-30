609ace2c00c507129bffcd1c0c496a779b57af41
raza ayaz <razaayaz@yahoo.com>
2024-12-31 00:32:45 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+// var cityName = prompt ('enter your city','city name');
+var question = "Your gender";
+ var defaultAnswer = "male";
+ 
+ var spec = prompt(question, defaultAnswer);
+
+
+
+if(spec == "male"  ){
+     document.write('GOOD MORNING SIR')
+}
+else {
+    document.write("GOOD MORNING MAAM")
+}
